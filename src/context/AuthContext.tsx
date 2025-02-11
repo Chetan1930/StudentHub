318277432d215a08b967f7ebca23a0c3943ee4da
Mayank Rawat <131944775+rawatshahab@users.noreply.github.com>
2025-02-11 22:52:47 +0530

    signup

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -35,7 +35,7 @@ export function AuthProvider({ children }) {
           fullname: name,
           photo: "",
         });
-        toast.success("Sign-up successful!");
+        
         window.location.href = "/";
       })
       .catch((err) => {
